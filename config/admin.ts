e866889c1c7c9648b36e76d7b26a290f5c4d29f9
Kimberly <work223305@gmail.com>
2024-10-23 10:38:16 +0800

    Token Fix

--- config/admin.ts
+++ config/admin.ts
@@ -1,13 +1,13 @@
 export default ({ env }) => ({
   auth: {
-    secret: env("16cc37b1a32ee3411d5b73309b89f0e5dc17a1256e7a6aa2110b54f3771956d5"),
+    secret: env("ADMIN_JWT_SECRET"),
   },
   apiToken: {
-    salt: env("183f5bcfb45272d3160153518d77dfdd6bacf2f1cc7f3ee3865219fad5822f7d"),
+    salt: env("API_TOKEN_SALT"),
   },
   transfer: {
     token: {
-      salt: env("e090de5f09a4896c976935921ee7cd16e3882c65b80be64ca5e4fa4f66c5f6c1"),
+      salt: env("TRANSFER_TOKEN_SALT"),
     },
   },
   flags: {
